30edaae42ee02a902c9578d6795c250632312d78
Lawll.n <92900098+nthi2003@users.noreply.github.com>
2022-10-14 18:06:24 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+
+  $('.name-menu').slick(
+    {
+        prevArrow:`<button type='button' class='slick-prev pull-left'><i class="fa-solid fa-arrow-left"></i></button>`,
+        nextArrow:`<button type='button' class='slick-next pull-right'><i class="fa-sharp fa-solid fa-arrow-right"></i></button>`,
+
+    }
+  );
